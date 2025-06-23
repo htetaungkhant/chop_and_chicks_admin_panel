97a70e0586f56634174d14653ee8149577a1706d
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-06-23 19:19:49 +0700

    Implement password visibility toggle in login form

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Loader2 } from "lucide-react";
+import { Eye, EyeClosed, Loader2 } from "lucide-react";
 
 export function LoginForm() {
   const router = useRouter();
@@ -22,6 +22,7 @@ export function LoginForm() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -69,14 +70,30 @@ export function LoginForm() {
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input
-                  id="password"
-                  type="password"
-                  required
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  disabled={loading}
-                />
+                <div className="relative">
+                  <Input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="********"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled={loading}
+                    className="pr-10"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 right-0 flex items-center px-3"
+                    disabled={loading}
+                  >
+                    {!showPassword ? (
+                      <EyeClosed className="h-5 w-5 text-gray-400 cursor-pointer" />
+                    ) : (
+                      <Eye className="h-5 w-5 text-gray-400 cursor-pointer" />
+                    )}
+                  </button>
+                </div>
               </div>
               {error && <p className="text-red-500 text-sm">{error}</p>}
               <Button type="submit" className="w-full" disabled={loading}>
